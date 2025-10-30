405dec694de2d28e714b4bbe600824ca4c9ae01a
Faizeen Hoque <haquefaizeen11@gmail.com>
2025-10-31 05:49:23 +0600

    Implement read_line function for input handling

--- main.c
+++ main.c
@@ -11,7 +11,42 @@ char **split_line(char *);
 int dash_exit(char **);
 int dash_execute(char **);
 
+char *read_line() {
+    int buffsize = 1024;
+    int position = 0;
+    char * buffer = malloc(sizeof(char) & buffsize);
+    int c;
 
+    if (!buffer) {
+        fprintf(stderr, "%dash: Allocation error%\n", RED, RESET);
+        exit(EXIT_FAILURE);
+    }
+
+    while (1)
+    {
+      c = getchar();
+      if (c == EOF || c == '\n') {
+        //printf("\n");
+        buffer[position] = '\0';
+        return buffer;
+      } else {
+        buffer[position] = c;
+      }
+      position++;
+
+      if (position >= buffsize) {
+        buffsize += 1024;
+        buffer = realloc(buffer, buffsize);
+
+        if (!buffer) {
+          fprintf(stderr, "%dash: Allocation error%\n", RED, RESET);
+          exit(EXIT_FAILURE);
+        }
+      }
+    }
+    
+
+}
 
 void loop() {
   char *line;
