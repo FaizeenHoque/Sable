724ea75de5d7709fed3f2e280f46bd3d2596bd03
Faizeen Hoque <haquefaizeen11@gmail.com>
2025-10-31 06:07:35 +0600

    Implement dash_execute function for command execution and update split_line function

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+// #include <sys/types.h>
+#include <unistd.h>
+// #include <sys/wait.h>
+
+#include <windows.h>
 
 #define TOK_DELIM " \t\r\n"
 #define RED "\033[0;31m"
@@ -10,6 +15,113 @@ char *read_line();
 char **split_line(char *);
 int dash_exit(char **);
 int dash_execute(char **);
+
+// int dash_execute(char **args) {
+//   pid_t cpid;
+//   int status;
+
+//   if (strcmp(args[0], "exit") == 0) {
+//     return dash_exit(args);
+//   }
+
+//   cpid = fork();
+
+//   if (cpid == 0) {
+//     if (execvp(args[0], args) < 0)
+//       printf("%sdash: Command not found: %s%s\n", RED, args[0], RESET);
+//     exit(EXIT_FAILURE);
+//   } else if (cpid < 0) {
+//     printf("%sdash: Error forking%s\n", RED, RESET);
+//   } else {
+//     waitpid(cpid, &status, WUNTRACED);
+//   }
+
+//   return 1;
+// }
+
+int dash_execute(char **args) {
+    if (strcmp(args[0], "exit") == 0) {
+        return dash_exit(args);
+    }
+
+    // Build command line for CreateProcess
+    char cmdline[1024] = "";
+    for (int i = 0; args[i] != NULL; i++) {
+        strcat(cmdline, args[i]);
+        if (args[i + 1] != NULL)
+            strcat(cmdline, " ");
+    }
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    // Create the process
+    if (!CreateProcess(
+            NULL,          // Application name
+            cmdline,       // Command line
+            NULL,          // Process handle not inheritable
+            NULL,          // Thread handle not inheritable
+            FALSE,         // Set handle inheritance to FALSE
+            0,             // No creation flags
+            NULL,          // Use parent's environment block
+            NULL,          // Use parent's starting directory 
+            &si,           // Pointer to STARTUPINFO structure
+            &pi)           // Pointer to PROCESS_INFORMATION structure
+    ) {
+        printf("%sdash: Command not found: %s%s\n", RED, args[0], RESET);
+        return 1;
+    }
+
+    // Wait until child process exits
+    WaitForSingleObject(pi.hProcess, INFINITE);
+
+    // Close process and thread handles
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 1;
+}
+
+int dash_exit(char **args)
+{
+	return 0;
+}
+
+char **split_line(char *line) {
+  int buffsize = 1024, position = 0;
+  char **tokens = malloc(buffsize * sizeof(char*));
+  char *token;
+
+  if (!tokens) {
+    fprintf(stderr, "%dash: Allocation error%\n", RED, RESET);
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position ++;
+
+    if (position >= buffsize) {
+      buffsize += 1024;
+      tokens = realloc(tokens, buffsize * sizeof(char*));
+      if (!tokens) {
+        fprintf(stderr, "%dash: Allocation error%\n", RED, RESET);
+        exit(EXIT_FAILURE);
+      }
+
+      token = strtok(NULL, TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+
+    return tokens;
+  }
+}
 
 char *read_line() {
     int buffsize = 1024;
